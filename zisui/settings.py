7af9a2fb4259145b8c0e6d60c7291fb6becf8d26
TanakaKazushiro <tnk30125146@gmail.com>
2023-04-18 13:22:07 +0900

    4/18

--- zisui/settings.py
+++ zisui/settings.py
@@ -111,8 +111,8 @@ DATABASES = {
 }
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_redquire=True)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600, ssl_redquire=True)
+# DATABASES['default'].update(db_from_env)
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
