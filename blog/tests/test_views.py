2f7e363807a772818e7666bc88b42ed083326dab
TanakaKazushiro <tnk30125146@gmail.com>
2023-04-27 21:26:36 +0900

    4/27

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-# class PostCreateViewTests(TestCase):
-#   """PostCreateViewのテストクラス"""
+class PostCreateViewTests(TestCase):
+  """PostCreateViewのテストクラス"""
 
-#   def test_get(self):
-#     """GET メソッドでアクセスしてステータスコード200を返されることを確認"""
-#     response = self.client.get(reverse('post'))
-#     self.assertEqual(response.status_code, 302)
+  def test_get(self):
+    """GET メソッドでアクセスしてステータスコード302を返されることを確認"""
+    response = self.client.get(reverse('post'))
+    self.assertEqual(response.status_code, 302)
--- login/tests/test_views.py
+++ login/tests/test_views.py
@@ -6,6 +6,6 @@ class MypageViewTests(TestCase):
   """MypageViewのテストクラス"""
 
   def test_get(self):
-    """GET メソッドでアクセスしてステータスコード200を返されることを確認"""
+    """GET メソッドでアクセスしてステータスコード302を返されることを確認"""
     response = self.client.get(reverse('mypage'))
     self.assertEqual(response.status_code, 302)
